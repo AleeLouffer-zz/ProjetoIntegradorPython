9299d2f7559dc10e468dc62359ac35e33fdcbdda
camilavitoriacosta <69814341+camilavitoriacosta@users.noreply.github.com>
2021-10-14 14:53:32 -0400

    Adicionando Teste do Agendamento

---
+++ TesteAgendamento.py
@@ -0,0 +1,62 @@
+import unittest
+from datetime import datetime
+from Agendamento import Agendamento
+#Adicionar importação das classes de Servico, ....
+
+class TesteFuncionario(unittest.TestCase):
+  def test_Deve_criar_um_agendamento(self):
+    servico = Servico()
+    funcionario = Funcionario()
+    cliente = Cliente()
+    empresa = Empresa()
+    dataEHora = datetime.strptime('08/10/2021 09:22', '%d/%m/%Y %H:%M')
+    agendamento = Agendamento(servico, funcionario,cliente, empresa, dataEHora)
+
+    assert agendamento.servico == servico
+    assert agendamento.funcionario == funcionario
+    assert agendamento.cliente == cliente
+    assert agendamento.empresa == empresa
+    assert agendamento.dataEHora == dataEHora
+  
+  def test_Nao_deve_criar_um_agendamento_sem_servico(self):
+    servico = Servico()
+    funcionario = Funcionario()
+    cliente = Cliente()
+    empresa = Empresa()
+    dataEHora = datetime.strptime('08/10/2021 09:22', '%d/%m/%Y %H:%M')
+    with self.assertRaises(ValueError):  
+      agendamento = Agendamento(None, funcionario, cliente, empresa, dataEHora)
+      
+  def test_Nao_deve_criar_um_agendamento_sem_funcionario(self):
+     servico = Servico()
+     cliente = Cliente()
+     empresa = Empresa()
+     dataEHora = datetime.strptime('08/10/2021 09:22', '%d/%m/%Y %H:%M')
+     with self.assertRaises(ValueError):  
+       agendamento = Agendamento(servico, None, cliente, empresa, dataEHora)
+      
+  def test_Nao_deve_criar_um_agendamento_sem_cliente(self):
+     servico = Servico()
+     funcionario = Funcionario()
+     empresa = Empresa()
+     dataEHora = datetime.strptime('08/10/2021 09:22', '%d/%m/%Y %H:%M')
+     with self.assertRaises(ValueError):  
+       agendamento = Agendamento(servico, funcionario, None, empresa, dataEHora)
+      
+  def test_Nao_deve_criar_um_agendamento_sem_empresa(self):
+     servico = Servico()
+     funcionario = Funcionario()
+     cliente = Cliente()
+     dataEHora = datetime.strptime('08/10/2021 09:22', '%d/%m/%Y %H:%M')
+     with self.assertRaises(ValueError):  
+       agendamento = Agendamento(servico, funcionario, cliente, None, dataEHora)
+      
+  def test_Nao_deve_criar_um_agendamento_sem_data_e_hora(self):
+     servico = Servico()
+     funcionario = Funcionario()
+     cliente = Cliente()
+     empresa = Empresa()
+     with self.assertRaises(ValueError):  
+       agendamento = Agendamento(servico, funcionario, cliente, empresa, None)
+      
+
