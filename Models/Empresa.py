f8d5027473704145cae9b0a3af3bd26352b1d002
Alexandro Ferreira <alexandrolouffer@hotmail.com>
2021-10-14 15:32:30 -0400

    Adicionando Empresa e Testes

---
+++ Models/Empresa.py
@@ -0,0 +1,93 @@
+class Empresa:
+    def __init__(self, nomeFantasia, razaoSocial, cnpj, agendamentos, servicos, funcionarios, email, senha):
+        self.Validar(nomeFantasia, razaoSocial, cnpj, agendamentos, servicos, funcionarios, email, senha)
+        self.__nomeFantasia = nomeFantasia
+        self.__razaoSocial = razaoSocial
+        self.__cnpj__ = cnpj
+        self.__agendamentos = agendamentos
+        self.__servicos = servicos
+        self.__funcionarios = funcionarios
+        self.__email = email
+        self.__senha = senha
+
+    def Validar(self, nomeFantasia, razaoSocial, __cnpj__, agendamentos, servicos, funcionarios, email, senha):
+        if not nomeFantasia or nomeFantasia.isspace():
+            raise ValueError("Nome Fantasia da empresa invalido")
+        if not razaoSocial or razaoSocial.isspace():
+            raise ValueError("Razao Social da empresa invalida")
+        if not __cnpj__ or __cnpj__.isspace():
+            raise ValueError("CNPJ da empresa invalido")
+        if not agendamentos:
+            raise ValueError("Agendamento Invalido")
+        if not servicos:
+            raise ValueError("Servicos Invalidos")
+        if not funcionarios:
+            raise ValueError("funcionarios Invalidos")
+        if not email or email.isspace():
+            raise ValueError("Email da empresa invalido")
+        if not senha or senha.isspace():
+            raise ValueError("Senha da empresa invalida")
+
+    @property
+    def nomeFantasia(self):
+        return self.__nomeFantasia
+
+    @nomeFantasia.setter
+    def nomeFantasia(self, nomeFantasia):
+        self.__nomeFantasia = nomeFantasia
+
+    @property
+    def razaoSocial(self):
+        return self.__razaoSocial
+
+    @razaoSocial.setter
+    def razaoSocial(self, razaoSocial):
+        self.__razaoSocial = razaoSocial
+
+    @property
+    def cnpj(self):
+        return self.__cnpj__
+
+    @cnpj.setter
+    def cnpj(self, cnpj):
+        self.__cnpj__ = cnpj
+
+    @property
+    def agendamentos(self):
+        return self.__agendamentos
+
+    @agendamentos.setter
+    def agendamentos(self, agendamentos):
+        self.__agendamentos = agendamentos
+
+    @property
+    def servicos(self):
+        return self.__servicos
+
+    @servicos.setter
+    def servicos(self, servicos):
+        self.__servicos = servicos
+
+    @property
+    def funcionarios(self):
+        return self.__funcionarios
+
+    @funcionarios.setter
+    def funcionarios(self, funcionarios):
+        self.__funcionarios = funcionarios
+
+    @property
+    def email(self):
+        return self.__email
+
+    @email.setter
+    def email(self, email):
+        self.__email = email
+
+    @property
+    def senha(self):
+        return self.__senha
+
+    @senha.setter
+    def senha(self, senha):
+        self.__senha = senha
---
+++ Testes/TesteEmpresa.py
@@ -0,0 +1,52 @@
+import unittest
+from Models.Empresa import Empresa
+
+
+class TesteEmpresa(unittest.TestCase):
+    def test_Deve_criar_uma_empresa(self):
+        empresa = Empresa("Dell", "DellComputadores", "21032193210", "agendamentos", "servicos", "funcionarios", "Dell@gmail.com", "EuOdeioPython")
+        assert empresa.nomeFantasia == "Dell"
+        assert empresa.razaoSocial == "DellComputadores"
+        assert empresa.cnpj == "21032193210"
+        assert empresa.agendamentos == "agendamentos"
+        assert empresa.servicos == "servicos"
+        assert empresa.funcionarios == "funcionarios"
+        assert empresa.email == "Dell@gmail.com"
+        assert empresa.senha == "EuOdeioPython"
+
+    def test_Nao_deve_criar_uma_empresa_com_nome_invalido(self):
+        with self.assertRaises(ValueError):
+            empresa = Empresa(None, "DellComputadores", "21032193210", "agendamentos", "servicos", "funcionarios",
+                          "Dell@gmail.com", "EuOdeioPython")
+
+    def test_Nao_deve_criar_uma_empresa_com_razao_social_invalida(self):
+        with self.assertRaises(ValueError):
+            empresa = Empresa("Dell", None, "21032193210", "agendamentos", "servicos", "funcionarios", "Dell@gmail.com", "EuOdeioPython")
+
+    def test_Nao_deve_criar_uma_empresa_com_CNPJ_invalido(self):
+        with self.assertRaises(ValueError):
+            empresa = Empresa("Dell", "DellComputadores", None, "agendamentos", "servicos", "funcionarios", "Dell@gmail.com", "EuOdeioPython")
+
+    def test_Nao_deve_criar_uma_empresa_com_agendamentos_invalidos(self):
+        with self.assertRaises(ValueError):
+            empresa = Empresa("Dell", "DellComputadores", "21032193210", None, "servicos", "funcionarios", "Dell@gmail.com", "EuOdeioPython")
+
+    def test_Nao_deve_criar_uma_empresa_com_servicos_invalidos(self):
+        with self.assertRaises(ValueError):
+            empresa = Empresa("Dell", "DellComputadores", "21032193210", "agendamentos", None, "funcionarios", "Dell@gmail.com", "EuOdeioPython")
+
+    def test_Nao_deve_criar_uma_empresa_com_funcionarios_invalidos(self):
+        with self.assertRaises(ValueError):
+            empresa = Empresa("Dell", "DellComputadores", "21032193210", "agendamentos", "servicos", None, "Dell@gmail.com", "EuOdeioPython")
+
+    def test_Nao_deve_criar_uma_empresa_com_email_invalido(self):
+        with self.assertRaises(ValueError):
+            empresa = Empresa("Dell", "DellComputadores", "21032193210", "agendamentos", "servicos", "funcionarios", None, "EuOdeioPython")
+
+    def test_Nao_deve_criar_uma_empresa_com_senha(self):
+        with self.assertRaises(ValueError):
+            empresa = Empresa("Dell", "DellComputadores", "21032193210", "agendamentos", "servicos", "funcionarios", "Dell@gmail.com", None)
+
+
+if __name__ == '__main__':
+    unittest.main()
