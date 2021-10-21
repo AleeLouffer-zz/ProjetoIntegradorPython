19db321246eda5a7cecfcd1c6d305083ca83e130
Alexandro Ferreira <alexandrolouffer@hotmail.com>
2021-10-21 15:26:58 -0400

    Corrigindo PR

--- Models/Empresa.py
+++ Models/Empresa.py
@@ -2,34 +2,26 @@ from validate_docbr import CNPJ
 
 
 class Empresa:
-    def __init__(self, nomeFantasia, razaoSocial, cnpj, agendamentos, servicos, funcionarios, email, senha):
-        self.Validar(nomeFantasia, razaoSocial, cnpj, agendamentos, servicos, funcionarios, email, senha)
+    def __init__(self, nomeFantasia, razaoSocial, cnpj, email, senha):
+        self.Validar(nomeFantasia, razaoSocial, cnpj, email, senha)
         self.__nomeFantasia = nomeFantasia
         self.__razaoSocial = razaoSocial
         self.__cnpj__ = cnpj
-        self.__agendamentos = agendamentos
-        self.__servicos = servicos
-        self.__funcionarios = funcionarios
         self.__email = email
         self.__senha = senha
 
-    def Validar(self, nomeFantasia, razaoSocial, __cnpj__, agendamentos, servicos, funcionarios, email, senha):
+    @staticmethod
+    def Validar(nomeFantasia, razaoSocial, cnpj, email, senha):
         if not nomeFantasia or nomeFantasia.isspace():
-            raise ValueError("Nome Fantasia da empresa invalido")
+            raise ValueError("Nome Fantasia da empresa vazio ou nulo")
         if not razaoSocial or razaoSocial.isspace():
-            raise ValueError("Razao Social da empresa invalida")
-        if not __cnpj__ or __cnpj__.isspace() or not CNPJ().validate(__cnpj__):
-            raise ValueError("CNPJ da empresa invalido")
-        if not agendamentos:
-            raise ValueError("Agendamento Invalido")
-        if not servicos:
-            raise ValueError("Servicos Invalidos")
-        if not funcionarios:
-            raise ValueError("funcionarios Invalidos")
+            raise ValueError("Razao Social da empresa vazio ou nulo")
+        if not cnpj or cnpj.isspace() or not CNPJ().validate(cnpj):
+            raise ValueError("CNPJ da empresa vazio ou nulo")
         if not email or email.isspace():
-            raise ValueError("Email da empresa invalido")
+            raise ValueError("Email da empresa vazio ou nulo")
         if not senha or senha.isspace():
-            raise ValueError("Senha da empresa invalida")
+            raise ValueError("Senha da empresa vazio ou nulo")
 
     @property
     def nomeFantasia(self):
@@ -54,30 +46,6 @@ class Empresa:
     @cnpj.setter
     def cnpj(self, cnpj):
         self.__cnpj__ = cnpj
-
-    @property
-    def agendamentos(self):
-        return self.__agendamentos
-
-    @agendamentos.setter
-    def agendamentos(self, agendamentos):
-        self.__agendamentos = agendamentos
-
-    @property
-    def servicos(self):
-        return self.__servicos
-
-    @servicos.setter
-    def servicos(self, servicos):
-        self.__servicos = servicos
-
-    @property
-    def funcionarios(self):
-        return self.__funcionarios
-
-    @funcionarios.setter
-    def funcionarios(self, funcionarios):
-        self.__funcionarios = funcionarios
 
     @property
     def email(self):
--- Testes/TesteEmpresa.py
+++ Testes/TesteEmpresa.py
@@ -3,54 +3,44 @@ from Models.Empresa import Empresa
 
 
 class TesteEmpresa(unittest.TestCase):
+    nomeFantasia = "Dell"
+    razaoSocial = "DellComputadores"
+    cnpj = "06868396000170"
+    email = "Dell@gmail.com"
+    senha = "EuOdeioPython"
+
     def test_Deve_criar_uma_empresa(self):
-        empresa = Empresa("Dell", "DellComputadores", "06868396000170", "agendamentos", "servicos", "funcionarios", "Dell@gmail.com", "EuOdeioPython")
+        empresa = Empresa("Dell", "DellComputadores", "06868396000170", "Dell@gmail.com", "EuOdeioPython")
         assert empresa.nomeFantasia == "Dell"
         assert empresa.razaoSocial == "DellComputadores"
         assert empresa.cnpj == "06868396000170"
-        assert empresa.agendamentos == "agendamentos"
-        assert empresa.servicos == "servicos"
-        assert empresa.funcionarios == "funcionarios"
         assert empresa.email == "Dell@gmail.com"
         assert empresa.senha == "EuOdeioPython"
 
-    def test_Nao_deve_criar_uma_empresa_com_nome_invalido(self):
+    def test_Nao_deve_criar_uma_empresa_com_nome_vazio_ou_nulo(self):
         with self.assertRaises(ValueError):
-            empresa = Empresa(None, "DellComputadores", "21032193210", "agendamentos", "servicos", "funcionarios",
-                          "Dell@gmail.com", "EuOdeioPython")
+            empresa = Empresa(None, self.razaoSocial, self.cnpj, self.email, self.senha)
 
-    def test_Nao_deve_criar_uma_empresa_com_razao_social_invalida(self):
+    def test_Nao_deve_criar_uma_empresa_com_razao_social_vazio_ou_nulo(self):
         with self.assertRaises(ValueError):
-            empresa = Empresa("Dell", None, "21032193210", "agendamentos", "servicos", "funcionarios", "Dell@gmail.com", "EuOdeioPython")
+            empresa = Empresa(self.nomeFantasia, None, self.cnpj, self.email, self.senha)
 
-    def test_Nao_deve_criar_uma_empresa_com_CNPJ_vazio(self):
+    def test_Nao_deve_criar_uma_empresa_com_CNPJ_vazio_ou_nulo(self):
         with self.assertRaises(ValueError):
-            empresa = Empresa("Dell", "DellComputadores", None, "agendamentos", "servicos", "funcionarios", "Dell@gmail.com", "EuOdeioPython")
+            empresa = Empresa(self.nomeFantasia, self.razaoSocial, None, self.email, self.senha)
 
     def test_Nao_deve_criar_uma_empresa_com_CNPJ_invalido(self):
+        cnpjInvalido = "89493000117"
         with self.assertRaises(ValueError):
-            empresa = Empresa("Dell", "DellComputadores", "89493000117", "agendamentos", "servicos", "funcionarios", "Dell@gmail.com", "EuOdeioPython")
+            empresa = Empresa(self.nomeFantasia, self.razaoSocial, cnpjInvalido, self.email, self.senha)
 
-
-    def test_Nao_deve_criar_uma_empresa_com_agendamentos_invalidos(self):
+    def test_Nao_deve_criar_uma_empresa_com_email_vazio_ou_nulo(self):
         with self.assertRaises(ValueError):
-            empresa = Empresa("Dell", "DellComputadores", "21032193210", None, "servicos", "funcionarios", "Dell@gmail.com", "EuOdeioPython")
+            empresa = Empresa(self.nomeFantasia, self.razaoSocial, self.cnpj, None, self.senha)
 
-    def test_Nao_deve_criar_uma_empresa_com_servicos_invalidos(self):
+    def test_Nao_deve_criar_uma_empresa_com_vazio_ou_nulo(self):
         with self.assertRaises(ValueError):
-            empresa = Empresa("Dell", "DellComputadores", "21032193210", "agendamentos", None, "funcionarios", "Dell@gmail.com", "EuOdeioPython")
-
-    def test_Nao_deve_criar_uma_empresa_com_funcionarios_invalidos(self):
-        with self.assertRaises(ValueError):
-            empresa = Empresa("Dell", "DellComputadores", "21032193210", "agendamentos", "servicos", None, "Dell@gmail.com", "EuOdeioPython")
-
-    def test_Nao_deve_criar_uma_empresa_com_email_invalido(self):
-        with self.assertRaises(ValueError):
-            empresa = Empresa("Dell", "DellComputadores", "21032193210", "agendamentos", "servicos", "funcionarios", None, "EuOdeioPython")
-
-    def test_Nao_deve_criar_uma_empresa_com_senha(self):
-        with self.assertRaises(ValueError):
-            empresa = Empresa("Dell", "DellComputadores", "21032193210", "agendamentos", "servicos", "funcionarios", "Dell@gmail.com", None)
+            empresa = Empresa(self.nomeFantasia, self.razaoSocial, self.cnpj, self.email, None)
 
 
 if __name__ == '__main__':
