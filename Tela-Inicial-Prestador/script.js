da7ef6f9f558b9412240d5f9132ca4cd38bc6723
DanielZanelato <DanielZanelato@digix.com.br>
2021-11-05 16:50:52 -0400

    Criando-tela

---
+++ Tela-Inicial-Prestador/script.js
@@ -0,0 +1,17 @@
+function ExibirCadastroFuncionario() {
+    var x = document.getElementById("checkBoxFunc");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+    
+  }
+  function ExibirCadastroServico(){
+    var x = document.getElementById("checkBoxServ");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
