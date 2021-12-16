a4bb8d322f5306f660f7c675427e86ccbba2a32a
AleeLouffer <aleelouffer@gmail.com>
2021-12-16 14:42:38 -0400

    Criando entidades no banco

--- PIPython/Agendador/models.py
+++ PIPython/Agendador/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
-# Create your models here.
+class Agendamento(models.Model):
+    id = models.AutoField(primary_key=True)
+    servico = models.ForeignKey('Servico', on_delete=models.CASCADE)
+    funcionario = models.ForeignKey('Funcionario', on_delete=models.CASCADE)
+    cliente = models.ForeignKey('Cliente', on_delete=models.CASCADE)
+    empresa = models.ForeignKey('Empresa', on_delete=models.CASCADE)
+    dataEHora = models.DateField(null=True)
+  
+
+class Cliente(models.Model):
+    id = models.AutoField(primary_key=True)
+    nome = models.CharField(max_length = 100)
+    telefone = models.CharField(max_length = 11)
+
+
+class Empresa(models.Model):      
+    id = models.AutoField(primary_key=True)
+    razaoSocial = models.CharField(max_length=100)
+    cnpj = models.IntegerField(max_length=15)
+    email = models.CharField(max_length=40)
+    senha = models.CharField(max_length=20)
+
+
+class Funcionario(models.Model):
+    id = models.AutoField(primary_key=True)
+    nome = models.CharField(max_length = 100)
+
+  
+class Servico(models.Model):
+    id = models.AutoField(primary_key=True)
+    nome = models.CharField(max_length = 100)
+    descricao = models.TextField()
+    preco = models.FloatField()
+    empresa = models.ForeignKey('Empresa', on_delete=models.CASCADE)
