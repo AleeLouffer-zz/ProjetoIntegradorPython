63295b3aef8dfbaecb5650b07becf2823db1d266
Jean Menezes <jeanvbonimenezes@gmail.com>
2022-02-18 15:52:12 -0300

    Movendo editar empresa para views da tela inicial

--- PIPython/Agendador/views/views_tela_inicial.py
+++ PIPython/Agendador/views/views_tela_inicial.py
@@ -23,6 +23,20 @@ def obter_funcionarios(id_empresa):
 def obter_servicos(id_empresa):
     return Servico.objects.filter(empresa_id = id_empresa)
 
+def editarEmpresa(requisicao):
+    id_empresa = requisicao.POST['id_empresa']
+    nome_empresa = requisicao.POST['nome_empresa']
+    email_empresa = requisicao.POST['email_empresa']
+    senha_empresa = requisicao.POST['senha_empresa']
+
+    empresa = Empresa.objects.get(id = id_empresa)
+    empresa.nome_fantasia = nome_empresa
+    empresa.email = email_empresa
+    empresa.senha = senha_empresa
+    empresa.save()
+
+    return redirect('tela_inicial_prestador', id_empresa = id_empresa)
+
 def editar_funcionario(requisicao):
     id_funcionario = requisicao.POST['id_funcionario']
     nome_novo = requisicao.POST['nome_funcionario']
