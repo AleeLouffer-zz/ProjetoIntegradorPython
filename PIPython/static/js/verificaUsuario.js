668a873231ec2e545c63bd4e8dc263636b4b4e56
Camila Vitoria <camilavitoria.c.costa@gmail.com>
2022-03-12 19:42:59 -0400

    Alterando front-end + mascara de cpf e cnpj

---
+++ PIPython/static/js/verificaUsuario.js
@@ -0,0 +1,21 @@
+function pessoaFisica(event){
+    event.preventDefault();
+    mudaUsuario("Física", "CPF");
+    var cnpj_cpf = document.getElementById('cnpj_cpf');
+    cnpj_cpf.setAttribute('maxlength','14');
+    cnpj_cpf.value = converteCPF(cnpj_cpf.value.slice(0,14));
+}
+
+function pessoaJuridica(event){
+    event.preventDefault();
+    mudaUsuario("Juridica", "CNPJ");
+    var cnpj_cpf = document.getElementById('cnpj_cpf');
+    cnpj_cpf.setAttribute('maxlength','18');
+    cnpj_cpf.value = converteCNPJ(cnpj_cpf.value);
+}
+
+function mudaUsuario(pessoa, cpf_cnpj){
+    document.getElementById('nome').placeholder = "Digite o nome da Pessoa " + pessoa;
+    document.getElementById('label_cnpj_cpf').innerHTML = cpf_cnpj;
+    document.getElementById('cnpj_cpf').placeholder = "Digite o " + cpf_cnpj;
+}
