478f246eda97ad3ad11dbd4fc9eacc97950feb20
Camila Vitoria <camilavitoria.c.costa@gmail.com>
2022-03-23 21:09:23 -0400

    Arrumando detalhes

--- PIPython/static/js/verificaUsuario.js
+++ PIPython/static/js/verificaUsuario.js
@@ -3,6 +3,7 @@ function pessoaFisica(event){
     mudaUsuario("Física", "CPF");
     var cnpj_cpf = document.getElementById('cnpj_cpf');
     cnpj_cpf.setAttribute('maxlength','14');
+    cnpj_cpf.setAttribute('minlength','14');
     cnpj_cpf.value = converteCPF(cnpj_cpf.value.slice(0,14));
 }
 
@@ -11,6 +12,7 @@ function pessoaJuridica(event){
     mudaUsuario("Juridica", "CNPJ");
     var cnpj_cpf = document.getElementById('cnpj_cpf');
     cnpj_cpf.setAttribute('maxlength','18');
+    cnpj_cpf.setAttribute('minlength','18');
     cnpj_cpf.value = converteCNPJ(cnpj_cpf.value);
 }
 
